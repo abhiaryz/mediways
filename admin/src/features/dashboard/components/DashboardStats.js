e672fe24a356df4d8d6c55e82185ba86da159977
Ganeshghatti <ghattiganesh8@gmail.com>
2024-07-27 19:07:20 +0530

    fixed dashboard

--- admin/src/features/dashboard/components/DashboardStats.js
+++ admin/src/features/dashboard/components/DashboardStats.js
@@ -1,23 +1,39 @@
-function DashboardStats({title, icon, value, description, colorIndex}){
+import { Link } from "react-router-dom";
 
-    const COLORS = ["primary", "primary"]
+function DashboardStats({ path, iconlarge, name, desc, colorIndex }) {
+  const COLORS = ["primary", "primary"];
 
-    const getDescStyle = () => {
-        if(description.includes("↗︎"))return "font-bold text-green-700 dark:text-green-300"
-        else if(description.includes("↙"))return "font-bold text-rose-500 dark:text-red-400"
-        else return ""
-    }
+  const getDescStyle = () => {
+    if (description.includes("↗︎"))
+      return "font-bold text-green-700 dark:text-green-300";
+    else if (description.includes("↙"))
+      return "font-bold text-rose-500 dark:text-red-400";
+    else return "";
+  };
 
-    return(
-        <div className="stats shadow">
-            <div className="stat">
-                <div className={`stat-figure dark:text-slate-300 text-${COLORS[colorIndex%2]}`}>{icon}</div>
-                <div className="stat-title dark:text-slate-300">{title}</div>
-                <div className={`stat-value dark:text-slate-300 text-${COLORS[colorIndex%2]}`}>{value}</div>
-                <div className={"stat-desc  " + getDescStyle()}>{description}</div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="stats shadow py-4">
+      <Link to={`${path}`}>
+        <div className="stat">
+          <div
+            className={`stat-figure text-xl dark:text-slate-300 text-${
+              COLORS[colorIndex % 2]
+            }`}
+          >
+            {iconlarge}
+          </div>
+          <div className="stat-title dark:text-slate-300 break-words whitespace-normal">{desc}</div>
+          <div
+            className={`stat-value dark:text-slate-300 text-${
+              COLORS[colorIndex % 2]
+            }`}
+          >
+            {name}
+          </div>
+        </div>{" "}
+      </Link>
+    </div>
+  );
 }
 
-export default DashboardStats
+export default DashboardStats;
--- admin/src/routes/sidebar.js
+++ admin/src/routes/sidebar.js
@@ -1,56 +1,62 @@
 /** Icons are imported separatly to reduce build time */
-import Squares2X2Icon from '@heroicons/react/24/outline/Squares2X2Icon'
-import WalletIcon from '@heroicons/react/24/outline/WalletIcon'
-import UserIcon from '@heroicons/react/24/outline/UserIcon'
-import Cog6ToothIcon from '@heroicons/react/24/outline/Cog6ToothIcon'
-import CurrencyDollarIcon from '@heroicons/react/24/outline/CurrencyDollarIcon'
-import InboxArrowDownIcon from '@heroicons/react/24/outline/InboxArrowDownIcon'
-import UsersIcon from '@heroicons/react/24/outline/UsersIcon'
+import Squares2X2Icon from "@heroicons/react/24/outline/Squares2X2Icon";
+import WalletIcon from "@heroicons/react/24/outline/WalletIcon";
+import UserIcon from "@heroicons/react/24/outline/UserIcon";
+import Cog6ToothIcon from "@heroicons/react/24/outline/Cog6ToothIcon";
+import CurrencyDollarIcon from "@heroicons/react/24/outline/CurrencyDollarIcon";
+import InboxArrowDownIcon from "@heroicons/react/24/outline/InboxArrowDownIcon";
+import UsersIcon from "@heroicons/react/24/outline/UsersIcon";
 
-const iconClasses = `h-6 w-6`
-const submenuIconClasses = `h-5 w-5`
+const iconClasses = `h-6 w-6`;
+const iconLargeClasses = `h-10 w-10`;
+const submenuIconClasses = `h-5 w-5`;
 
 const routes = [
-
   {
-    path: '/app/dashboard',
-    icon: <Squares2X2Icon className={iconClasses}/>, 
-    name: 'Dashboard',
+    path: "/app/dashboard",
+    icon: <Squares2X2Icon className={iconClasses} />,
+    iconlarge: <Squares2X2Icon className={iconLargeClasses} />,
+    name: "Dashboard",
+    desc: 'Home'
   },
   {
-    path: '/app/Specialities', // url
-    icon: <InboxArrowDownIcon className={iconClasses}/>, // icon component
-    name: 'Specialities', // name that appear in Sidebar
+    path: "/app/Specialities", // url
+    icon: <InboxArrowDownIcon className={iconClasses} />, // icon component    
+    iconlarge: <InboxArrowDownIcon className={iconLargeClasses} />, // icon component
+    name: "Specialities", // name that appear in Sidebar
+    desc: "Create new or Modify exisiting Speciality",
   },
   {
-    path: '/app/campaigns', // url
-    icon: <CurrencyDollarIcon className={iconClasses}/>, // icon component
-    name: 'Campaigns', // name that appear in Sidebar
+    path: "/app/campaigns", // url
+    icon: <CurrencyDollarIcon className={iconClasses} />, // icon component
+    iconlarge: <CurrencyDollarIcon className={iconLargeClasses} />, // icon component
+    name: "Campaigns", // name that appear in Sidebar
+    desc: "Create new or Modify exisiting Campaign",
   },
   {
-    path: '', //no url needed as this has submenu
-    icon: <Cog6ToothIcon className={`${iconClasses} inline` }/>, // icon component
-    name: 'Settings', // name that appear in Sidebar
-    submenu : [
+    path: "/app/settings", //no url needed as this has submenu
+    icon: <Cog6ToothIcon className={`${iconClasses} inline`} />, // icon component
+    iconlarge: <Cog6ToothIcon className={`${iconLargeClasses} inline`} />, // icon component
+    name: "Settings", // name that appear in Sidebar
+    desc: "Modify settings for admin panel",
+    submenu: [
       {
-        path: '/app/settings-profile', //url
-        icon: <UserIcon className={submenuIconClasses}/>, // icon component
-        name: 'Profile', // name that appear in Sidebar
+        path: "/app/settings-profile", //url
+        icon: <UserIcon className={submenuIconClasses} />, // icon component
+        name: "Profile", // name that appear in Sidebar
       },
       {
-        path: '/app/settings-billing',
-        icon: <WalletIcon className={submenuIconClasses}/>,
-        name: 'Billing',
+        path: "/app/settings-billing",
+        icon: <WalletIcon className={submenuIconClasses} />,
+        name: "Billing",
       },
       {
-        path: '/app/settings-team', // url
-        icon: <UsersIcon className={submenuIconClasses}/>, // icon component
-        name: 'Team Members', // name that appear in Sidebar
+        path: "/app/settings-team", // url
+        icon: <UsersIcon className={submenuIconClasses} />, // icon component
+        name: "Team Members", // name that appear in Sidebar
       },
-    ]
-  },  
-]
+    ],
+  },
+];
 
-export default routes
-
-
+export default routes;
