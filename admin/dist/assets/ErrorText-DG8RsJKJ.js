d0e0b96c5c9e76b5d125a9dc861655a6c952307b
Ganeshghatti <ghattiganesh8@gmail.com>
2024-07-27 21:45:30 +0530

    added base:'./' in vite.config.js

---
+++ admin/dist/assets/ErrorText-DG8RsJKJ.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-vvvWIYqA.js";function s({styleClass:r,children:t}){return e.jsx("p",{className:`text-center  text-error ${r}`,children:t})}export{s as E};
---
+++ admin/dist/assets/Subtitle-1WpuQ5SU.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-vvvWIYqA.js";function i({styleClass:t,children:s}){return e.jsx("div",{className:`text-xl font-semibold ${t}`,children:s})}export{i as S};
