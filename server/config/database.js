d50a7a585b0e6e2e9eef9e19f34f6ba617dcf9d3
Ganeshghatti <ghattiganesh8@gmail.com>
2024-07-27 21:23:07 +0530

    directly added mongo URI to connect db

--- server/config/database.js
+++ server/config/database.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 
 const connectdatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_CONNECT_URI);
+    // await mongoose.connect(process.env.MONGODB_CONNECT_URI);
+    await mongoose.connect(
+      "mongodb+srv://ganeshghatti6:tBVJRtseC1Xfczbm@mediways.5bserjh.mongodb.net/?retryWrites=true&w=majority&appName=mediways"
+    );
+
     console.log("db connection successful");
-    
   } catch (error) {
     console.log("db connection failed" + error.message);
   }
