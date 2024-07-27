fd1dd9999b03d5cc1c3690b4b0b17a613cb63b06
Ganeshghatti <ghattiganesh8@gmail.com>
2024-07-27 21:45:51 +0530

    added base:'./' in vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   //added global:window to fix draft-js issue. Error solved: "global not defined"
   // define: {
   //   global: "window",
