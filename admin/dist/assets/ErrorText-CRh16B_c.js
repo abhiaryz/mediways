59e9f93c32fc5e99bc2d373ce24b0d7f48b99fc2
Ganeshghatti <ghattiganesh8@gmail.com>
2024-07-31 13:53:39 +0530

    added correct vercel.json script

--- admin/dist/assets/ErrorText-CRh16B_c.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-B1WDzP11.js";function s({styleClass:r,children:t}){return e.jsx("p",{className:`text-center  text-error ${r}`,children:t})}export{s as E};
--- admin/dist/assets/Subtitle-B4toLw5U.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-B1WDzP11.js";function i({styleClass:t,children:s}){return e.jsx("div",{className:`text-xl font-semibold ${t}`,children:s})}export{i as S};
