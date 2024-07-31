f519cd98f5412befd88a45926921014217be5669
Ganeshghatti <ghattiganesh8@gmail.com>
2024-07-31 14:17:48 +0530

    added services to admin panel

---
+++ admin/dist/assets/Subtitle-7zIYQHEd.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Bf73z5r4.js";function i({styleClass:t,children:s}){return e.jsx("div",{className:`text-xl font-semibold ${t}`,children:s})}export{i as S};
--- admin/dist/assets/Subtitle-DKnovVMo.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DVbhMGLa.js";function i({styleClass:t,children:s}){return e.jsx("div",{className:`text-xl font-semibold ${t}`,children:s})}export{i as S};
