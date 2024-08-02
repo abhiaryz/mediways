2e7a43579424c4423f289b3282822836a009fecb
Ganeshghatti <ghattiganesh8@gmail.com>
2024-08-02 20:22:54 +0530

    trying to fix MIME issue

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://saram7619.github.io/deploying-vite-project-example/",
+  base: "https://aidcirle-admin.vercel.app",
   //added global:window to fix draft-js issue. Error solved: "global not defined"
   // define: {
   //   global: "window",
