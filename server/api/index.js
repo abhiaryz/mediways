b0d7a49f46874bb1a415b807995bbe6e9575a321
GaneshGhatti <ganeshghatti6@gmail.com>
2024-12-10 19:59:50 +0530

    initial commit

--- server/api/index.js
+++
@@ -1,8 +0,0 @@
-const app = require("../main");
-const mongoose = require('mongoose');
-
-const PORT = process.env.PORT;
-
-const server = app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
